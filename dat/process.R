769b9f5b65338c53f584185f4628d1b4232ddc12
Arthur Lui <luiarthur@gmail.com>
2016-05-19 16:04:47 -0700

    yes

---
+++ dat/process.R
@@ -0,0 +1,28 @@
+library(maps) # map()
+library(LatticeKrig) # quilt.plot()
+x <- as.matrix( read.table("HadISST1_SST_2015.txt") )
+xvec <- c(x)
+xvec[which(xvec != -1000)] <- xvec[which(xvec != -1000)] / 100
+xvec[which(xvec == -1000)] <- -500
+
+n <- nrow(x)
+k <- ncol(x)
+
+1:n
+
+lat <- rep(seq(89.5,-89.5,length=k),times=n)#rep(1:k,each=n)
+lon <- rep(seq(-179.5,179.5,length=n),each=k)#rep(1:n,k)
+
+
+latlon<- cbind(lat,lon)
+
+viewPred <- function(x,latlon,main.plot='',bks=range(x),leg=T) {
+  quilt.plot(latlon[,2],latlon[,1],x,add.legend=leg,
+             fg='grey90',bty='n',main=main.plot,
+             ylim=range(latlon[,1])+c(-1,1),
+             xlim=range(latlon[,2])+c(-1,1),
+             breaks=seq(bks[1],bks[2],len=length(x)+1),
+             col= colorRampPalette(c('dark blue','grey90','dark red'))(length(x)))
+  map('world',add=T,col='grey')
+}
+viewPred(xvec,latlon)
